4da2173e6ecf3c7be1351ae7d335487ecca02cde
oscilloscode <88482930+oscilloscode@users.noreply.github.com>
2024-11-07 20:57:33 +0100

    create inst with path

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,29 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+/// DirHash
+///
+/// Test list:
+/// - Create instance with path which gets canonicalized
+/// - Create instance returns error is path doesn't exist
+/// - Get files im empty dir returns nothing
+/// - Get files in flat dir (no folders)
+/// - Get files in deep dir (multiple levels)
+/// - Get all file types (regular, link, char device, block device, socket, pipe)
+/// - Getting files doesn't follow symlinks
+/// - Compute hash of file (file, link, char device, block device, socket, pipe)
+/// - Compute hash of multiple files
+/// - Compute hash of list of hashes
+///
+use std::path::{Path, PathBuf};
+
+pub struct DirHash {
+    path: PathBuf,
+}
+
+impl DirHash {
+    pub fn new(path: &Path) -> Result<Self, std::io::Error> {
+        Ok(Self {
+            path: path.canonicalize()?,
+        })
+    }
 }
 
 #[cfg(test)]
@@ -7,8 +31,20 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn create_inst_exists() {
+        let dh = DirHash::new(Path::new("/tmp")).expect("Existing path resulted in error.");
+        assert_eq!(dh.path.to_str().unwrap(), "/tmp");
+    }
+
+    #[test]
+    fn create_inst_doesnt_exist() {
+        let dh = DirHash::new(Path::new("/xyz"));
+        assert!(dh.is_err());
+    }
+
+    #[test]
+    fn create_inst_canonicalize() {
+        let dh = DirHash::new(Path::new("/tmp/..")).expect("Error while canonicalizing path.");
+        assert_eq!(dh.path.to_str().unwrap(), "/");
     }
 }
