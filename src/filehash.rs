4fe10caf64a1fda6e98c79303ffeb2f243fd96cf
oscilloscode <88482930+oscilloscode@users.noreply.github.com>
2024-11-30 17:00:25 +0100

    creating distinct tempfiles for every test

--- src/filehash.rs
+++ src/filehash.rs
@@ -14,69 +14,102 @@ impl FileHash {
 
 #[cfg(test)]
 mod tests {
-    use std::{
-        fs::File,
-        io::{Read, Seek, Write},
-        sync::{Mutex, MutexGuard, OnceLock},
-    };
+    use std::collections::HashMap;
+    use std::io::{Read, Seek, Write};
+    use std::sync::OnceLock;
 
     use super::*;
     use tempfile::NamedTempFile;
 
-    static INIT: OnceLock<Mutex<TestFile>> = OnceLock::new();
-
-    struct TestFile {
-        file: NamedTempFile,
-        path: PathBuf,
+    #[derive(Clone, Debug, Hash, PartialEq, PartialOrd, Eq, Ord)]
+    struct TestVector {
+        content: String,
         hash: String,
     }
 
-    fn init_file() -> MutexGuard<'static, TestFile> {
-        let tempfile = INIT.get_or_init(|| {
-            let mut file = NamedTempFile::new().expect("Can't create tempfile");
-            write!(&mut file, "tempfile data").expect("Can't write to tempfile");
-            file.rewind().expect("Can't rewind file");
-            let testfile = TestFile {
-                path: file.path().to_owned(),
-                file,
-                hash: String::from("lkjsdf"),
-            };
-            testfile.into()
-        });
-        let mut mg = tempfile.lock().unwrap();
-        mg.file.rewind().expect("Can't rewind file");
-        mg
+    #[derive(Debug)]
+    struct TestFile {
+        file: NamedTempFile,
+        test_vector: TestVector,
     }
 
-    #[test]
-    fn init_creates_tempfile() {
-        let mut mg = init_file();
-        let mut file_content = String::new();
-        mg.file
-            .read_to_string(&mut file_content)
-            .expect("Couldn't read file.");
-        assert_eq!(file_content, "tempfile data");
-        assert_eq!(mg.hash, "lkjsdf");
+    #[derive(Clone, Copy, Debug, Hash, PartialEq, PartialOrd, Eq, Ord)]
+    enum TestFileContent {
+        Empty,
+        SingleLine,
+        MultiLine,
     }
 
-    #[test]
-    fn multiple_inits_rewind_tempfile() {
-        let mut testfile = init_file();
+    fn get_testvector_hashmap() -> &'static HashMap<TestFileContent, TestVector> {
+        static HASHMAP: OnceLock<HashMap<TestFileContent, TestVector>> = OnceLock::new();
+        HASHMAP.get_or_init(|| {
+            let mut m = HashMap::new();
+            m.insert(
+                TestFileContent::Empty,
+                TestVector {
+                    content: String::from(""),
+                    hash: String::from(
+                        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
+                    ),
+                },
+            );
+            m.insert(
+                TestFileContent::SingleLine,
+                TestVector {
+                    content: String::from("First line"),
+                    hash: String::from(
+                        "2361df1018e7458967cc1e554069bdfb1e8ecaad33db0462806129f81ebb6a8a",
+                    ),
+                },
+            );
+            m.insert(
+                TestFileContent::MultiLine,
+                TestVector {
+                    content: String::from("First line\nSecond line\nThird line\n"),
+                    hash: String::from(
+                        "10441734233b9dd30cabeed4511c8e5f56e67cffc1d37a2fcbefca8532cd34f2",
+                    ),
+                },
+            );
+            m
+        })
+    }
+
+    fn get_testfile(content: TestFileContent) -> TestFile {
+        let mut file = NamedTempFile::new().expect("Can't create tempfile");
+
+        let test_vector = get_testvector_hashmap().get(&content).unwrap().to_owned();
+        write!(&mut file, "{}", &test_vector.content).expect("Can't write to tempfile");
+        file.rewind().expect("Can't rewind file");
+
+        TestFile { file, test_vector }
+    }
+
+    fn check_testfile(content: TestFileContent) {
+        let mut testfile = get_testfile(content);
+        let test_vector = get_testvector_hashmap().get(&content).unwrap().to_owned();
         let mut file_content = String::new();
         testfile
             .file
             .read_to_string(&mut file_content)
             .expect("Couldn't read file.");
-        assert_eq!(file_content, "tempfile data");
-        file_content.clear();
-        assert_eq!(file_content, "");
+        assert_eq!(test_vector.content, file_content);
+        assert_eq!(test_vector.hash, testfile.test_vector.hash);
+    }
 
-        let mut testfile = init_file();
-        testfile
-            .file
-            .read_to_string(&mut file_content)
-            .expect("Couldn't read file.");
-        assert_eq!(file_content, "tempfile data");
+    #[test]
+    fn get_testfile_empty() {
+        check_testfile(TestFileContent::Empty);
+    }
+
+    #[test]
+    fn get_testfile_singleline() {
+        check_testfile(TestFileContent::SingleLine);
+    }
+
+    #[test]
+    fn get_testfile_multiline() {
+        check_testfile(TestFileContent::MultiLine);
     }
 
     // #[test]
