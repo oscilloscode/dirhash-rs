129cbca178b7a260020a10594a6df2f97c4d250b
oscilloscode <88482930+oscilloscode@users.noreply.github.com>
2024-12-28 11:53:38 +0100

    pathhashlist: convert pathhash stub to spy

--- src/pathhashlist.rs
+++ src/pathhashlist.rs
@@ -30,15 +30,32 @@ mod tests {
 
     use super::*;
 
-    struct PathHashStub {
+    // Todo:
+    // - Create a new function and force calling it by wrapping spy into "instrumentation" module?
+    struct PathHashSpy {
         path: PathBuf,
         hash: Option<[u8; 32]>,
+        next_hash: Option<[u8; 32]>,
+        call_count_compute_hash: u32,
     }
 
-    impl PathHashProvider for PathHashStub {
+    impl PathHashSpy {
+        fn call_count_compute_hash(&self) -> u32 {
+            self.call_count_compute_hash
+        }
+    }
+
+    impl PathHashProvider for PathHashSpy {
         fn compute_hash(&mut self) -> Result<(), std::io::Error> {
-            Ok(())
-            // Err(std::io::Error::new(std::io::ErrorKind::Other, "oh no!"))
+            self.call_count_compute_hash += 1;
+
+            match self.next_hash {
+                Some(hash) => {
+                    self.hash = Some(hash);
+                    Ok(())
+                }
+                None => Err(std::io::Error::new(std::io::ErrorKind::NotFound, "oh no!")),
+            }
         }
 
         fn hash(&self) -> Option<&[u8; 32]> {
@@ -51,33 +68,91 @@ mod tests {
     }
 
     #[test]
-    fn create_stubs_for_pathhashlist() {
-        let stubs = vec![
-            PathHashStub {
+    fn create_pathhashprovider_spies() {
+        let mut spies = vec![
+            PathHashSpy {
                 path: Path::new("/some/path").to_owned(),
                 hash: None,
+                next_hash: None,
+                call_count_compute_hash: 0,
             },
-            PathHashStub {
+            PathHashSpy {
                 path: Path::new("/other/path").to_owned(),
                 hash: Some(*b"01234567890123456789012345678901"),
+                next_hash: None,
+                call_count_compute_hash: 0,
             },
         ];
-        let mut pathhashlist = PathHashList::new(stubs).expect("Can't create PathHashList");
 
-        assert!(pathhashlist.files[0].compute_hash().is_ok());
-        assert_eq!("/some/path", pathhashlist.files[0].path().to_str().unwrap());
-        assert!(pathhashlist.files[0].hash().is_none());
-        assert_eq!(
-            "/other/path",
-            pathhashlist.files[1].path().to_str().unwrap()
-        );
-        assert_eq!(0x34, pathhashlist.files[1].hash().unwrap()[4]);
+        assert_eq!("/some/path", spies[0].path().to_str().unwrap());
+        assert!(spies[0].hash().is_none());
+        assert_eq!(0, spies[0].call_count_compute_hash());
+        assert_eq!("/other/path", spies[1].path().to_str().unwrap());
+        assert_eq!(0x34, spies[1].hash().unwrap()[4]);
+        assert_eq!(0, spies[1].call_count_compute_hash());
+    }
+
+    #[test]
+    fn spy_compute_hash() {
+        let mut spy = PathHashSpy {
+            path: Path::new("/some/path").to_owned(),
+            hash: None,
+            next_hash: Some(*b"01234567890123456789012345678901"),
+            call_count_compute_hash: 0,
+        };
+
+        assert!(spy.compute_hash().is_ok());
+
+        assert_eq!(1, spy.call_count_compute_hash());
+        assert_eq!(b"01234567890123456789012345678901", spy.hash().unwrap());
+        assert_eq!(b"01234567890123456789012345678901", &spy.next_hash.unwrap());
+    }
+
+    // TODO:
+    // - Use something different than std::io::Error to accomodate the error thrown here? But,
+    // changing the error type just for the test is probably bad. Check if the type also profits
+    // from changing the error type.
+    #[test]
+    fn spy_compute_hash_no_nexthash() {
+        let mut spy = PathHashSpy {
+            path: Path::new("/some/path").to_owned(),
+            hash: None,
+            next_hash: None,
+            call_count_compute_hash: 0,
+        };
+
+        let e = spy.compute_hash();
+        assert!(e.is_err());
+        assert_eq!(e.unwrap_err().kind(), std::io::ErrorKind::NotFound);
+        assert_eq!(1, spy.call_count_compute_hash());
+    }
+
+    #[test]
+    fn spy_compute_hash_later_no_nexthash() {
+        let mut spy = PathHashSpy {
+            path: Path::new("/some/path").to_owned(),
+            hash: None,
+            next_hash: Some(*b"01234567890123456789012345678901"),
+            call_count_compute_hash: 0,
+        };
+
+        assert!(spy.compute_hash().is_ok());
+
+        assert_eq!(b"01234567890123456789012345678901", spy.hash().unwrap());
+        assert_eq!(b"01234567890123456789012345678901", &spy.next_hash.unwrap());
+        assert_eq!(1, spy.call_count_compute_hash());
+
+        spy.next_hash = None;
+        let e = spy.compute_hash();
+        assert!(e.is_err());
+        assert_eq!(e.unwrap_err().kind(), std::io::ErrorKind::NotFound);
+        assert_eq!(2, spy.call_count_compute_hash());
     }
 
     #[test]
     fn pathhashlist_hash_is_none_after_init() {
-        let stubs: Vec<PathHashStub> = vec![];
-        let pathhashlist = PathHashList::new(stubs).expect("Can't create PathHashList");
+        let spies: Vec<PathHashSpy> = vec![];
+        let pathhashlist = PathHashList::new(spies).expect("Can't create PathHashList");
         assert!(pathhashlist.hash.is_none());
     }
 }
