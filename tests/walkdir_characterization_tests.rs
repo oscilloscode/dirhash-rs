419f79a251101e024a0d5184810d1a029864e5b7
oscilloscode <88482930+oscilloscode@users.noreply.github.com>
2025-01-25 19:24:50 +0100

    walkdir tests: files and folder structure

--- tests/walkdir_characterization_tests.rs
+++ tests/walkdir_characterization_tests.rs
@@ -1,25 +1,64 @@
 //! * Rationale
 //!
-//! These characterization tests intend to capture my assumptions about the walkdir crate and they function as a visual reference. Additionally, this prevents me from
-//! overlooking a breaking change by resulting in test failures.
+//! These characterization tests intend to capture my assumptions about the walkdir crate and they
+//! function as a visual reference. Additionally, this prevents me from overlooking a breaking
+//! change by resulting in test failures.
+//!
+//! Things to check:
+//! - Listing all files on multiple levels
+//! - List a lot of files
+//! - Listing hidden files
+//! - Listing files with special characters in filename
+//! - How to handle symlinks?
 
-use std::fs::File;
-use std::io::Write;
-use tempfile::tempdir;
+use std::{fs::File, path::Path};
+use tempfile::{tempdir, TempDir};
+use walkdir::WalkDir;
 
-#[test]
-fn creating_tempdir() {
-    let dir = tempdir().expect("Can't create tempdir");
-    let file_path = dir.path().join("my-temporary-note.txt");
-    let mut file = File::create(file_path).expect("Error while creating file");
-    writeln!(file, "Brian was here. Briefly.").expect("Error while writing file");
+fn create_numbered_files(dir: impl AsRef<Path>, n: usize) {
+    println!("Hello {:?}", dir.as_ref());
+    for i in 0..n {
+        let _ =
+            File::create(dir.as_ref().join(format!("{}", i))).expect("Error while creating file");
+    }
+}
+
+fn creating_tempdir() -> TempDir {
+    // let dir = tempdir().expect("Can't create tempdir");
+    let dir = tempfile::Builder::new()
+        .keep(true)
+        .tempdir()
+        .expect("Can't create tempdir");
+
+    create_numbered_files(&dir, 4);
+
+    for d in ["a", "b", "c"] {
+        let newdir = dir.path().join(d);
+        std::fs::create_dir(&newdir)
+            .expect(&format!("Error while creating directory {:?}", newdir));
+
+        create_numbered_files(&newdir, 6);
+    }
 
     println!("{:?}", dir.path());
-    // By closing the `TempDir` explicitly, we can check that it has
-    // been deleted successfully. If we don't close it explicitly,
-    // the directory will still be deleted when `dir` goes out
-    // of scope, but we won't know whether deleting the directory
-    // succeeded.
-    drop(file);
-    dir.close().expect("Can't close tempdir");
+
+    dir
+}
+
+#[test]
+fn this_works() {
+    let dir = creating_tempdir();
+
+    println!("within test {:?}", dir.path());
+    for entry in WalkDir::new(&dir)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| !e.file_type().is_dir())
+    {
+        println!("{:?}", entry);
+    }
+
+    assert_eq!(2 + 1, 3);
+
+    // dir.close().expect("Can't close tempdir");
 }
