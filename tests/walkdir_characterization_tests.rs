cd465d982660840e463c7a94d010e53ffb975c12
oscilloscode <88482930+oscilloscode@users.noreply.github.com>
2025-01-29 20:45:56 +0100

    walkdir tests: revised tempdir generation

--- tests/walkdir_characterization_tests.rs
+++ tests/walkdir_characterization_tests.rs
@@ -16,15 +16,14 @@ use tempfile::{tempdir, TempDir};
 use walkdir::WalkDir;
 
 fn create_numbered_files(dir: impl AsRef<Path>, n: usize) {
-    println!("Hello {:?}", dir.as_ref());
     for i in 0..n {
         let _ =
             File::create(dir.as_ref().join(format!("{}", i))).expect("Error while creating file");
     }
 }
 
-/// Creates the following directory structure for creating_tempdir(4, 3, 6, 3, 3):
-///
+/// Creates the following directory structure for creating_tempdir(4, &["a", "b", "c"][..], 6,
+/// &["x", "y", "z"][..], 3):
 /// ```
 /// tmpbSlLgw/
 /// ├── 0
@@ -91,9 +90,9 @@ fn create_numbered_files(dir: impl AsRef<Path>, n: usize) {
 /// ```
 fn creating_tempdir(
     l1_files: usize,
-    l1_dirs: usize,
+    l1_dirs: &[&str],
     l2_files: usize,
-    l2_dirs: usize,
+    l2_dirs: &[&str],
     l3_files: usize,
 ) -> TempDir {
     // let dir = tempdir().expect("Can't create tempdir");
@@ -104,14 +103,14 @@ fn creating_tempdir(
 
     create_numbered_files(&dir, l1_files);
 
-    for d in ('a'..='z').take(l1_dirs) {
+    for d in l1_dirs.iter() {
         let dir_level_1 = dir.path().join(d.to_string());
         std::fs::create_dir(&dir_level_1)
             .expect(&format!("Error while creating directory {:?}", dir_level_1));
 
         create_numbered_files(&dir_level_1, l2_files);
 
-        for d in ('a'..='z').rev().take(l2_dirs) {
+        for d in l2_dirs.iter() {
             let dir_level_2 = dir_level_1.join(d.to_string());
             std::fs::create_dir(&dir_level_2)
                 .expect(&format!("Error while creating directory {:?}", dir_level_2));
@@ -120,22 +119,19 @@ fn creating_tempdir(
         }
     }
 
-    println!("{:?}", dir.path());
-
     dir
 }
 
 #[test]
 fn this_works() {
-    let dir = creating_tempdir(4, 3, 6, 3, 3);
+    let dir = creating_tempdir(4, &["a", "b", "c"][..], 6, &["x", "y", "z"][..], 3);
 
-    println!("within test {:?}", dir.path());
     for entry in WalkDir::new(&dir)
         .into_iter()
         .filter_map(Result::ok)
         .filter(|e| !e.file_type().is_dir())
     {
-        println!("{:?}", entry);
+        println!("{:?}", entry.path().strip_prefix(&dir).unwrap());
     }
 
     // dir.close().expect("Can't close tempdir");
