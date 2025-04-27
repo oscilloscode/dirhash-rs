8edfc4ab4c42f17bfe8f69ba1a1d24b25719f2c2
oscilloscode <88482930+oscilloscode@users.noreply.github.com>
2025-04-27 19:52:29 +0200

    hashtable: sort entries

--- src/hashtable.rs
+++ src/hashtable.rs
@@ -38,6 +38,10 @@ impl HashTable {
     pub fn append(&mut self, mut entries: Vec<HashTableEntry>) {
         self.entries.append(&mut entries);
     }
+
+    pub fn sort(&mut self) {
+        self.entries.sort();
+    }
 }
 
 #[cfg(test)]
@@ -102,5 +106,148 @@ mod tests {
         assert_eq!(ht.entries[2].hash, [2; 32]);
         assert_eq!(ht.entries[3].path, "/path3");
         assert_eq!(ht.entries[3].hash, [3; 32]);
+    }
+
+    #[test]
+    fn sort_hash_first_byte() {
+        let v = vec![
+            HashTableEntry::new(
+                [
+                    1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0,
+                ],
+                String::from("/one"),
+            )
+            .unwrap(),
+            HashTableEntry::new(
+                [
+                    0xF, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0,
+                ],
+                String::from("/f"),
+            )
+            .unwrap(),
+            HashTableEntry::new(
+                [
+                    9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0,
+                ],
+                String::from("/nine"),
+            )
+            .unwrap(),
+            HashTableEntry::new(
+                [
+                    0xA, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0,
+                ],
+                String::from("/a"),
+            )
+            .unwrap(),
+            HashTableEntry::new(
+                [
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0,
+                ],
+                String::from("/zero"),
+            )
+            .unwrap(),
+        ];
+
+        let mut ht = HashTable::new();
+        ht.append(v);
+        ht.sort();
+
+        assert_eq!(ht.entries[0].path, "/zero");
+        assert_eq!(ht.entries[1].path, "/one");
+        assert_eq!(ht.entries[2].path, "/nine");
+        assert_eq!(ht.entries[3].path, "/a");
+        assert_eq!(ht.entries[4].path, "/f");
+    }
+
+    #[test]
+    fn sort_hash_last_byte() {
+        let v = vec![
+            HashTableEntry::new(
+                [
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 7,
+                ],
+                String::from("/seven"),
+            )
+            .unwrap(),
+            HashTableEntry::new(
+                [
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 0xD,
+                ],
+                String::from("/d"),
+            )
+            .unwrap(),
+            HashTableEntry::new(
+                [
+                    0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                    0, 0, 0, 0, 0, 2,
+                ],
+                String::from("/two"),
+            )
+            .unwrap(),
+        ];
+
+        let mut ht = HashTable::new();
+        ht.append(v);
+        ht.sort();
+
+        assert_eq!(ht.entries[0].path, "/two");
+        assert_eq!(ht.entries[1].path, "/seven");
+        assert_eq!(ht.entries[2].path, "/d");
+    }
+
+    #[test]
+    fn sort_hash_path() {
+        let v: Vec<HashTableEntry> = vec![
+            HashTableEntry::new([0; 32], String::from("ä_umlaut")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("8")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("\\backslash")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("\"quote")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("?question mark")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("T")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("_underscore")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("7")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("a")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("(parens)")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("|pipe")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("*asterisk")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("-hyphen")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("~tilde")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("<angle brackets>")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("{braces}")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("[brackets]")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("d")).unwrap(),
+            HashTableEntry::new([0; 32], String::from("B")).unwrap(),
+        ];
+
+        let mut ht = HashTable::new();
+        ht.append(v);
+        ht.sort();
+
+        assert_eq!(ht.entries[0].path, "\"quote");
+        assert_eq!(ht.entries[1].path, "(parens)");
+        assert_eq!(ht.entries[2].path, "*asterisk");
+        assert_eq!(ht.entries[3].path, "-hyphen");
+        assert_eq!(ht.entries[4].path, "7");
+        assert_eq!(ht.entries[5].path, "8");
+        assert_eq!(ht.entries[6].path, "<angle brackets>");
+        assert_eq!(ht.entries[7].path, "?question mark");
+        assert_eq!(ht.entries[8].path, "B");
+        assert_eq!(ht.entries[9].path, "T");
+        assert_eq!(ht.entries[10].path, "[brackets]");
+        assert_eq!(ht.entries[11].path, "\\backslash");
+        assert_eq!(ht.entries[12].path, "_underscore");
+        assert_eq!(ht.entries[13].path, "a");
+        assert_eq!(ht.entries[14].path, "d");
+        assert_eq!(ht.entries[15].path, "{braces}");
+        assert_eq!(ht.entries[16].path, "|pipe");
+        assert_eq!(ht.entries[17].path, "~tilde");
+        assert_eq!(ht.entries[18].path, "ä_umlaut");
     }
 }
