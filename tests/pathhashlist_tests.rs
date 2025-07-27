10507cc82ef4aa0af56e6f1c1aed31516e5664cc
oscilloscode <88482930+oscilloscode@users.noreply.github.com>
2025-07-27 15:24:54 +0200

    pathhashlist: check hashtable in all tests

--- tests/pathhashlist_tests.rs
+++ tests/pathhashlist_tests.rs
@@ -41,6 +41,21 @@ fn create_from_path_recursively_no_root() {
     // e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/b/y/1
     //
     // -> a425a6aa4a9585eeee8cd7f2398c0cefd4f5e25228b71d3161872618993d19b8
+    assert_eq!(
+        pathhashlist.hashtable().unwrap().to_string(),
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/a/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/a/x/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/a/x/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/a/y/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/a/y/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/b/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/b/x/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/b/x/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/b/y/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  /tmp/.tmp_create_from_path_recursively_no_root/b/y/1\n"
+    );
     assert_eq!(pathhashlist.hash().unwrap(), b"\xa4\x25\xa6\xaa\x4a\x95\x85\xee\xee\x8c\xd7\xf2\x39\x8c\x0c\xef\xd4\xf5\xe2\x52\x28\xb7\x1d\x31\x61\x87\x26\x18\x99\x3d\x19\xb8");
 
     dir.close().expect("Can't close tempdir");
@@ -70,6 +85,21 @@ fn create_from_path_recursively_with_root() {
     // e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b/y/1
     //
     // -> 725fa4e7c9d48001e1ff3a453d7edd51a8bbe9390c06b64393e06518461adfd5
+    assert_eq!(
+        pathhashlist.hashtable().unwrap().to_string(),
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./a/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./a/x/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./a/x/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./a/y/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./a/y/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b/x/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b/x/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b/y/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b/y/1\n"
+    );
     assert_eq!(pathhashlist.hash().unwrap(), b"\x72\x5f\xa4\xe7\xc9\xd4\x80\x01\xe1\xff\x3a\x45\x3d\x7e\xdd\x51\xa8\xbb\xe9\x39\x0c\x06\xb6\x43\x93\xe0\x65\x18\x46\x1a\xdf\xd5");
 
     dir.close().expect("Can't close tempdir");
--- tests/rs_vs_sh.rs
+++ tests/rs_vs_sh.rs
@@ -41,8 +41,8 @@ fn sh_with_command() {
         .arg("fd -t f --exec sha256sum | sort")
         .output()
         .expect("Command failed");
-    let hash_list = String::from_utf8_lossy(&hash_list_output.stdout);
-    eprintln!("{}", &hash_list);
+    let sh_hashtable_str = String::from_utf8_lossy(&hash_list_output.stdout);
+    eprintln!("{}", &sh_hashtable_str);
 
     // Inefficient (recalculation), but shouldn't be a problem for tests
     let rec_hash_output = Command::new("bash")
@@ -53,22 +53,22 @@ fn sh_with_command() {
         .output()
         .expect("Command failed");
     let rec_hash = String::from_utf8_lossy(&rec_hash_output.stdout);
-    eprintln!("{}", &rec_hash);
-
-    let complete_output = hash_list + rec_hash.clone();
-    eprintln!("{}", complete_output);
 
     let sh_hash_str = rec_hash
         .split_whitespace()
         .next()
         .expect("Couldn't extract the hash string from the sh output");
 
+    eprintln!("{}", &sh_hash_str);
+
     // Verification
     // ------------
 
     let rs_hash_str = hex::encode(pathhashlist.hash().unwrap());
+    let rs_hashtable_str = pathhashlist.hashtable().unwrap().to_string();
 
     assert_eq!(sh_hash_str, rs_hash_str);
+    assert_eq!(sh_hashtable_str, rs_hashtable_str);
 
     // Hash of various empty files in tree structure:
     //
@@ -91,6 +91,26 @@ fn sh_with_command() {
     // e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bcd,ty/y/1
     //
     // -> 6a4bcbda9920637f38d636ade37b28c81b638dee3ac8729819e39d63433fdc22
+    assert_eq!(
+        rs_hashtable_str,
+        "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b,foo/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b,foo/x/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b,foo/x/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b,foo/y/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./b,foo/y/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bc,pe/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bc,pe/x/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bc,pe/x/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bc,pe/y/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bc,pe/y/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bcd,ty/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bcd,ty/x/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bcd,ty/x/1\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bcd,ty/y/0\n\
+         e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855  ./bcd,ty/y/1\n"
+    );
     assert_eq!(
         rs_hash_str,
         "6a4bcbda9920637f38d636ade37b28c81b638dee3ac8729819e39d63433fdc22"
